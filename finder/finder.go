7746bc6e562556d9a93612cc4655e659ca0a1745
Max Baird <mmb1@hw.ac.uk>
2019-02-16 01:42:54 +0000

    The depth of traversal is being kept track of in the path struct

--- finder/finder.go
+++ finder/finder.go
@@ -10,6 +10,7 @@ import (
 
 type path struct {
 	root         int
+	depth        int
 	letters      []rune
 	traversePath map[int]struct{}
 }
@@ -26,59 +27,65 @@ func canMove(tile *tile.Tile, tilePath *path) bool {
 	return true
 }
 
-func traverse(tile *tile.Tile, p *path, depth int, wg *sync.WaitGroup) {
+func traverse(tile *tile.Tile, p *path, wg *sync.WaitGroup) {
 	if wg != nil {
 		defer wg.Done()
 	}
 
-	if depth == 9 {
-		return
+	if p != nil { //Will be nil on first call
+		if p.depth == 9 {
+			return
+		}
 	}
 
 	tilePath := &path{}
 	tilePath.letters = make([]rune, 9)
 	tilePath.traversePath = make(map[int]struct{})
 
-	if p != nil {
+	if p == nil { //Will be nil on first call
+		tilePath.root = int(tile.ID)
+	} else {
 		for k, v := range p.traversePath {
 			tilePath.traversePath[k] = v
 		}
-
 		copy(tilePath.letters, p.letters)
+		tilePath.depth = p.depth
+		tilePath.root = p.root
 	}
 
 	tilePath.traversePath[int(tile.ID)] = struct{}{}
-	tilePath.letters[depth] = tile.Letter
+	tilePath.letters[tilePath.depth] = tile.Letter
+	tilePath.depth++
 
-	word := string(tilePath.letters[0 : depth+1])
+	word := string(tilePath.letters[0:tilePath.depth])
 
 	if lexis.IsWord(word) {
-		fmt.Println(word)
+		fmt.Printf("%s:[%d]\n", word, tilePath.root)
 	}
 
 	if canMove(tile.N, tilePath) {
-		traverse(tile.N, tilePath, depth+1, nil)
+		traverse(tile.N, tilePath, nil)
 	}
 	if canMove(tile.S, tilePath) {
-		traverse(tile.S, tilePath, depth+1, nil)
+		traverse(tile.S, tilePath, nil)
 	}
 	if canMove(tile.E, tilePath) {
-		traverse(tile.E, tilePath, depth+1, nil)
+		traverse(tile.E, tilePath, nil)
 	}
 	if canMove(tile.W, tilePath) {
-		traverse(tile.W, tilePath, depth+1, nil)
+		traverse(tile.W, tilePath, nil)
 	}
 	if canMove(tile.NE, tilePath) {
-		traverse(tile.NE, tilePath, depth+1, nil)
+		traverse(tile.NE, tilePath, nil)
 	}
 	if canMove(tile.SE, tilePath) {
-		traverse(tile.SE, tilePath, depth+1, nil)
+		traverse(tile.SE, tilePath, nil)
 	}
 	if canMove(tile.SW, tilePath) {
-		traverse(tile.SW, tilePath, depth+1, nil)
+		traverse(tile.SW, tilePath, nil)
 	}
 	if canMove(tile.NW, tilePath) {
-		traverse(tile.NW, tilePath, depth+1, nil)
+		traverse(tile.NW, tilePath, nil)
 	}
 }
 
@@ -87,9 +94,12 @@ func FindWords(board *board.Board) {
 	fmt.Println("Finding...")
 	var wg sync.WaitGroup
 
-	wg.Add(len(board.Tiles))
-	for i := 0; i < 16; i++ {
-		go traverse(&board.Tiles[i], nil, 0, &wg)
+	numTiles := len(board.Tiles) //TODO make this a const in board
+
+	wg.Add(numTiles)
+
+	for i := 0; i < numTiles; i++ {
+		go traverse(&board.Tiles[i], nil, &wg)
 	}
 
 	wg.Wait()
