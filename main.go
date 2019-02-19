16ba2314850cb7dbae4d737f7d1eb2232971f281
Max Baird <mmb1@hw.ac.uk>
2019-02-19 09:51:38 +0000

    Removed profiling code

--- main.go
+++ main.go
@@ -5,46 +5,14 @@ import (
 	"astrid/configuration"
 	"astrid/lexis"
 	"astrid/strid"
-	"flag"
-	"log"
-	"os"
-	"runtime"
-	"runtime/pprof"
 )
-
-var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
-var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 const height int = 4
 const width int = 4
 
 func main() {
-	flag.Parse()
-	if *cpuprofile != "" {
-		f, err := os.Create(*cpuprofile)
-		if err != nil {
-			log.Fatal("could not create CPU profile: ", err)
-		}
-		if err := pprof.StartCPUProfile(f); err != nil {
-			log.Fatal("could not start CPU profile: ", err)
-		}
-		defer pprof.StopCPUProfile()
-	}
-
 	configuration.ReadConfig()
 	lexis.LoadLexis()
 	blitz := blitz.New(height, width)
 	blitz.Start()
-
-	if *memprofile != "" {
-		f, err := os.Create(*memprofile)
-		if err != nil {
-			log.Fatal("could not create memory profile: ", err)
-		}
-		runtime.GC() // get up-to-date statistics
-		if err := pprof.WriteHeapProfile(f); err != nil {
-			log.Fatal("could not write memory profile: ", err)
-		}
-		f.Close()
-	}
 }
